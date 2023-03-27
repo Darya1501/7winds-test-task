5ea63f6977da83b3298a3f2ec273b13be1cb3fa6
Darya <darya_01@mail.ru>
2023-03-27 13:35:21 +0300

    feature add navigation layout

--- src/components/layout/header/user.tsx
+++ src/components/layout/header/user.tsx
@@ -7,7 +7,7 @@ export const User = () => {
   return (
     <div className={styles.user}>
       <img src={userImage} alt="" className={styles.photo} />
-      <span>Антон Петров</span>
+      <span className={styles.name}>Антон Петров</span>
       <img src={arrow} alt="" />
     </div>
   )
