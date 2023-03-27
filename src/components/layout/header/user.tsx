894cd0a4764776467f6a17819f997ba70a697210
Darya <darya_01@mail.ru>
2023-03-27 13:04:16 +0300

    feature add header user block

--- src/components/layout/header/user.tsx
+++ src/components/layout/header/user.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import userImage from '../../../images/user.jpg'
+import arrow from '../../../images/arrow.svg'
+import styles from './header.module.sass'
 
 export const User = () => {
   return (
-    <div>User</div>
+    <div className={styles.user}>
+      <img src={userImage} alt="" className={styles.photo} />
+      <span>Антон Петров</span>
+      <img src={arrow} alt="" />
+    </div>
   )
 }
